c970d90e77cb567bef9292f903692eb44f81b032
jason <kwon_jason@yahoo.com>
2021-06-22 12:47:03 +1000

    change http verb to get

--- apis/tnxAnalyzer.js
+++ apis/tnxAnalyzer.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const analyzeTransaction = require('../utils/analyzer')
 
-router.post('/analyzeTransaction', async (req, res) => {
-  let tnxHash = req.body.hash
+router.get('/analyzeTransaction/:hash', async (req, res) => {
+  let tnxHash = req.params.hash
   let result = await analyzeTransaction(tnxHash)
   return res.json({
     result,
