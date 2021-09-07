89b1b82ffe63dfe729b3242a919f0fd05ec065a7
Light Fury <leech.developer@gmail.com>
2021-09-08 00:52:25 +1000

    - update gas price for current price

--- apis/gasOracle.js
+++ apis/gasOracle.js
@@ -2,9 +2,15 @@ const router = require('express').Router()
 const { getGasInfo } = require('../utils/variable');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   try {
-    res.status(200).send(getGasInfo());
+    const provider = new ethers.providers.JsonRpcProvider('https://rpc.ftm.tools/', 250);
+    const gasPrice = await provider.getGasPrice()
+    
+    res.status(200).send({
+      ...getGasInfo(),
+      gasPrice: new BigNumber(gasPrice.toNumber()).dividedBy(new BigNumber('1000000000')).toNumber()
+    });
   } catch (error) {
     console.log(error);
     res.status(404).send('respond with a error');
